49c735a5d4fac9757ce919b782ca746407720121
Nathaniel Donato <nathanieldonatofx@gmail.com>
2023-08-25 15:24:23 +0200

    Update footer.tsx

--- components/footer.tsx
+++ components/footer.tsx
@@ -9,30 +9,64 @@ export default function Footer() {
         <>
             <section>
                 <div>
-                    <h1 className="text-[20px]">The Instrumentalist</h1>
+                    <div>
+                        <h2>Contact Us</h2>
+                        <ul>
+                            <li>nathaniel@rndsquared.co.za</li>
+                            <li>067 114 4899</li>
+                        </ul>
+                    </div>
+                    <div>
+                        <h2>Explore</h2>
+                        <ul>
+                            <li>About</li>
+                            <li>Partners</li>
+                            <li>Job Opportunities</li>
+                            <li>Membership</li>
+                            <li>Advertise</li>
+                        </ul>
+                    </div>
+                    <div>
+                        <h2>Headquarters</h2>
+                        <ul>
+                            <li>16 Beachway, Durban North, Kwa-Zulu Natal South Africa 4051</li>
+                        </ul>
+                    </div>
+                    <div>
+                        <h2>Connections</h2>
+                        <div>
+                            <a href="https://web.facebook.com/rndsquared?_rdc=1&_rdr" className="mr-[20px]" target="_blank"><Image 
+                                src={fb}
+                                alt="Facebook Logo"
+                                width={20}
+                                height={20}/></a>
+                            <a href="https://web.facebook.com/rndsquared?_rdc=1&_rdr" className="mr-[20px]" target="_blank"><Image 
+                                src={insta}
+                                alt="Facebook Logo"
+                                width={20}
+                                height={20}/></a>
+                            <a href="https://web.facebook.com/rndsquared?_rdc=1&_rdr" className="mr-[20px]" target="_blank"><Image 
+                                src={x}
+                                alt="Facebook Logo"
+                                width={20}
+                                height={20}/></a>
+                            <a href="https://web.facebook.com/rndsquared?_rdc=1&_rdr" className="mr-[20px]" target="_blank"><Image 
+                                src={pin}
+                                alt="Facebook Logo"
+                                width={20}
+                                height={20}/></a>
+                        </div>
+                    </div>
                 </div>
                 <div>
-                    <a href="https://web.facebook.com/rndsquared?_rdc=1&_rdr" className="mr-[20px]" target="_blank"><Image 
-                        src={fb}
-                        alt="Facebook Logo"
-                        width={20}
-                        height={20}/></a>
-                    <a href="https://web.facebook.com/rndsquared?_rdc=1&_rdr" className="mr-[20px]" target="_blank"><Image 
-                        src={insta}
-                        alt="Facebook Logo"
-                        width={20}
-                        height={20}/></a>
-                    <a href="https://web.facebook.com/rndsquared?_rdc=1&_rdr" className="mr-[20px]" target="_blank"><Image 
-                        src={x}
-                        alt="Facebook Logo"
-                        width={20}
-                        height={20}/></a>
-                    <a href="https://web.facebook.com/rndsquared?_rdc=1&_rdr" className="mr-[20px]" target="_blank"><Image 
-                        src={pin}
-                        alt="Facebook Logo"
-                        width={20}
-                        height={20}/></a>
+                    <div>
+                        <h1 className="text-[20px]">The Instrumentalist | 2023</h1>
+                    </div>
+                    <div>
+                        <h1>Website by: Nathaniel Donato</h1>
+                    </div>
                 </div>
+                
             </section>
         </>
     )
