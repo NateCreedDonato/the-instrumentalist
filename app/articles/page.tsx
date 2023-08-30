9f62ddf8ac031dd5b59fe481e7e5e0c779756147
Nathaniel Donato <nathanieldonatofx@gmail.com>
2023-08-30 21:27:14 +0200

    Update page.tsx

--- app/articles/page.tsx
+++ app/articles/page.tsx
@@ -5,6 +5,7 @@ export default function Articles () {
         <>
             <h1>This is the Articles Page</h1>
             <h2>This is where articles will go</h2>
+            <h3>As well as another slider to show them all</h3>
         </>
         
     )
