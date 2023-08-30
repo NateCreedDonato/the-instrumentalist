e151b4f317cc4c4d1ff3a794ffb9e127e4bee3d1
Nathaniel Donato <nathanieldonatofx@gmail.com>
2023-08-30 21:29:33 +0200

    Update hero.tsx

--- app/components/hero.tsx
+++ app/components/hero.tsx
@@ -3,9 +3,13 @@ export default function Hero() {
         <>
             <section>
                 <div>
+                    <h1>Trending</h1>
+                </div>
+                <div>
                     <h1>This is the Hero slider</h1>
                     <h2>A slider with the best topics are gonna be put here.</h2>
                 </div>
+
             </section>
         </>
     )
