141f9f6d4b6de17c87643e7371f76a81430c0a8a
Nathaniel Donato <nathanieldonatofx@gmail.com>
2023-08-30 21:32:19 +0200

    Update page.tsx

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -8,6 +8,7 @@ export default function Contact() {
             </div>
             <div>
                 <h2>This is the Contact us Page, a page dedicated to showing you the user our contact information in  order for you to contact us.</h2>
+                <h3>Please contact us should you have any enquiries about our website or organisation.</h3>
             </div>
             <div>
                 <ul>
