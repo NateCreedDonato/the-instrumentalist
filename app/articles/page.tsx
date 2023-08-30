ba1bba4245085ee65be5b3d2ec9c3b32ecd04836
Nathaniel Donato <nathanieldonatofx@gmail.com>
2023-08-30 21:26:46 +0200

    Update page.tsx

--- app/articles/page.tsx
+++ app/articles/page.tsx
@@ -2,6 +2,10 @@ import Link from "next/link"
 
 export default function Articles () {
     return(
-        <h1>This is the Articles Page</h1>
+        <>
+            <h1>This is the Articles Page</h1>
+            <h2>This is where articles will go</h2>
+        </>
+        
     )
 }
