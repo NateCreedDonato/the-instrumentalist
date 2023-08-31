79f4a292a77d2dfae0f85b729683fcc1417d39d7
Nathaniel Donato <nathanieldonatofx@gmail.com>
2023-08-31 19:06:17 +0200

    Update page.tsx

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -12,6 +12,12 @@ export default function About () {
                 <h3>A safe space for musicians of all walks of life</h3>
                 <h4>Tell us your life story</h4>
             </div>
+            <div>
+                <h1>Who are we ?</h1>
+            </div>
+            <div>
+                <p>We are The Instrumentalist, an organisasion dedicated to bringing the latest on the music industry from the perspective of a Rock and Punk enjoyer</p>
+            </div>
         </>
     )
 }
