6ccf37fc2f05f17efe358bd84eaff3f39bdabcf3
Nathaniel Donato <nathanieldonatofx@gmail.com>
2023-08-31 12:54:00 +0200

    Update hero.tsx

--- app/components/hero.tsx
+++ app/components/hero.tsx
@@ -5,17 +5,10 @@ export default function Hero() {
     return(
         <>
             <section>
+                <div>Trending</div>
                 <div>
                     <Image src={pick2}
-                    alt="This is just a stand in"
-                    width="100%"/>
-                </div>
-                <div>
-                    <h1>Trending</h1>
-                </div>
-                <div>
-                    <h1>This is the Hero slider</h1>
-                    <h2>A slider with the best topics are gonna be put here.</h2>
+                    alt="This is just a stand in"/>
                 </div>
 
             </section>
