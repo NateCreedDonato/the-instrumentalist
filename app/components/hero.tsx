99fc564892c017b1a2310a8303980008267036e2
Nathaniel Donato <nathanieldonatofx@gmail.com>
2023-08-31 12:52:08 +0200

    Update hero.tsx

--- app/components/hero.tsx
+++ app/components/hero.tsx
@@ -1,7 +1,15 @@
+import Image from "next/image"
+import pick2 from "../../public/images/pick2.jpg"
+
 export default function Hero() {
     return(
         <>
             <section>
+                <div>
+                    <Image src={pick2}
+                    alt="This is just a stand in"
+                    width="100%"/>
+                </div>
                 <div>
                     <h1>Trending</h1>
                 </div>
