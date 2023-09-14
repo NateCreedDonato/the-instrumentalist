b82bc9dc1d59a8d2b85bd7bf53bbe0b2052b60ab
Nathaniel Donato <nathanieldonatofx@gmail.com>
2023-09-14 15:12:10 +0200

    Update popular.tsx

--- app/components/popular.tsx
+++ app/components/popular.tsx
@@ -20,13 +20,13 @@ export default function Popular() {
                         <Image src={pop1} 
                         alt="Nathaniel Donato on Guitar"
                         className="w-[310px] h-[280px] object-cover rounded-md]" />
-                        <div  className="mt-[30px] ml-[20px] w-[85%]">
+                        <div className="mt-[30px] ml-[20px] w-[85%]">
                             <h1 className="text-[18px] font-bold mb-[30px]">Nathaniel Donato on Guitar playing Techniques and Skills</h1>
                             <p className="text-third-grey text-[12px]">A bit of a random topic but something that people tend to have differing opinions on, let’s dive deep and find out both sides.</p>
                             <h2 className="mt-[30px] mb-[30px] text-[12px] font-bold">By Nathaniel Donato</h2>
                         </div>
                     </div>
-                    <div className="w-[310px]">
+                    <div className="w-[310px] mb-[30px] bg-white rounded-md">
                         <Image src={pop2} 
                         alt="Fender Telecaster"
                         className="w-[310px] h-[280px] object-cover rounded-md" />
@@ -35,7 +35,7 @@ export default function Popular() {
                             <h2>By Nathaniel Donato</h2>
                         </div>
                     </div>
-                    <div className="w-[310px]">
+                    <div className="w-[310px]mb-[30px] bg-white rounded-md">
                         <Image src={pop3} 
                         alt="Drummer on Drums"
                         className="w-[310px] h-[280px] object-cover rounded-md" />
@@ -44,7 +44,7 @@ export default function Popular() {
                             <h2>By Nathaniel Donato</h2>
                         </div>
                     </div>
-                    <div className="w-[310px]">
+                    <div className="w-[310px] mb-[30px] bg-white rounded-md">
                         <Image src={pop4} 
                         alt="Nord Keyboard"
                         className="w-[310px] h-[280px] object-cover rounded-md" />
@@ -53,7 +53,7 @@ export default function Popular() {
                             <h2>By Nathaniel Donato</h2>
                         </div>
                     </div>
-                    <div className="w-[310px]">
+                    <div className="w-[310px] mb-[30px] bg-white rounded-md">
                         <Image src={pop5} 
                         alt="Band performing live"
                         className="w-[310px] h-[280px] object-cover rounded-md" />
@@ -62,7 +62,7 @@ export default function Popular() {
                             <h2>By Nathaniel Donato</h2>
                         </div>
                     </div>
-                    <div className="w-[310px]">
+                    <div className="w-[310px] mb-[30px] bg-white rounded-md">
                         <Image src={pop6} 
                         alt="Empty Drum kit"
                         className="w-[310px] h-[280px] object-cover rounded-md" />
@@ -71,7 +71,7 @@ export default function Popular() {
                             <h2>By Nathaniel Donato</h2>
                         </div>
                     </div>
-                    <div className="w-[310px]">
+                    <div className="w-[310px] mb-[30px] bg-white rounded-md">
                         <Image src={pop7} 
                         alt="Fender Stratocaster on an Amp"
                         className="w-[310px] h-[280px] object-cover rounded-md" />
@@ -80,7 +80,7 @@ export default function Popular() {
                             <h2>By Nathaniel Donato</h2>
                         </div>               
                     </div>
-                    <div className="w-[310px]">
+                    <div className="w-[310px] mb-[30px] bg-white rounded-md">
                         <Image src={pop8} 
                         alt="Empty Keyboard"
                         className="w-[310px] h-[280px] object-cover rounded-md" />
