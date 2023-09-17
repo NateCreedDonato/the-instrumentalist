31d874b417c11d055ea45260bf9a0ec88ccceccf
Nathaniel Donato <nathanieldonatofx@gmail.com>
2023-09-17 13:42:27 +0200

    Next Auth added

---
+++ app/login/page.tsx
@@ -0,0 +1,11 @@
+import Link from "next/link"
+
+export default function Login() {
+    return(
+        <>
+            <div>
+                <h1>Login</h1>
+            </div>
+        </>
+    )
+}
