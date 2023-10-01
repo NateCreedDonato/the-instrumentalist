6d35bd4cc897ec831013f52f73f16b8c4425f29e
Nathaniel Donato <nathanieldonatofx@gmail.com>
2023-10-01 14:25:48 +0200

    Update page.tsx

--- app/articles/page.tsx
+++ app/articles/page.tsx
@@ -1,25 +1,16 @@
-import Link from "next/link"
-import Image from "next/image"
 import Header from "../components/header"
 import Footer from "../components/footer"
-import pop7 from "../../public/images/pop7.jpg"
+import Popular from "../components/popular"
+
 
 export default function Articles () {
     return(
         <>
-            <Header />
-            <div className="h-[600px] relative">
-                <Image src={pop7}
-                alt="This is just a stand in" fill
-                className="object-cover"/>
-                <div className="text-white absolute text-left top-[232px] ml-[80px] z-40">
-                    <h2 className="text-[46px] font-bold">Your Rig Matters</h2>
-                    <p className="text-[26px] font-semibold">What combo Rig should you go for?</p>
-                    <p className="text-[20px] font-bold">This crazy side of the music industry</p>
-                    <h2 className="text-[35px] font-bold text-sky-600">Just making changes</h2>
-                </div>
-            </div>
-            <Footer />
+            <section>
+                <Header />    
+                <Popular />
+                <Footer />
+            </section>
         </>
         
     )
